f359efa82e67b9ef9eaee7ba45b145e1fa65f729
rifgchael <111080673+rifgchael@users.noreply.github.com>
2022-09-09 20:05:47 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ def onmessage(update,bot:ObigramClient):
         #tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'Chris_bel14'
+        tl_admin_user = 'nautaii'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -960,9 +960,9 @@ def onmessage(update,bot:ObigramClient):
             #    bot_token = os.environ.get('bot_token')
             #    
                 # set in debug
-            #    api_id = 7386053
-            #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    api_id = 14681595
+            #    api_hash = 'a86730aab5c59953c424abb4396d32d5'
+            #    bot_token = '5759495332:AAGjdS7R0c3-VZFlhQbNJm4GKCSgdjEfQVo'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -975,7 +975,7 @@ def onmessage(update,bot:ObigramClient):
   
 
 def main():
-    bot_token = '5445756484:AAFkbYYA1lI7HC67VysbdljpZSlS7uRZp4A'
+    bot_token = '5759495332:AAGjdS7R0c3-VZFlhQbNJm4GKCSgdjEfQVo'
     
 
     bot = ObigramClient(bot_token)
